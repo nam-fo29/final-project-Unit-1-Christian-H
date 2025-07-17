6c1587cf750c24ad0fdeb45590d0e6491db39a29
Choffman <cmhoffman94@gmail.com>
2025-07-17 15:58:41 -0500

    First draft of the About page

---
+++ playback-final/src/pages/About.jsx
@@ -0,0 +1,12 @@
+//This component sets up the About page
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p className="about">Placeholder text for App write up and intro</p>
+        </div>
+    );
+}
+
+export default About
